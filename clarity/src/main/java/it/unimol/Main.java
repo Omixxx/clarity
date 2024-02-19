81c59afaa7e62deed7b5fea5cf4204e33f13315f
Omixxx <xxcortex99@gmail.com>
2024-02-19 10:14:28 +0100

    Moved the recursive call for directory tree exploration
    By moving the recursive call we can easily obtain the name of the pr
    oject under analysis, it is possible to create a temporary dire
    ctory without having conflicts with the path in which the project to be
    analyzed resides

--- clarity/src/main/java/it/unimol/Main.java
+++ clarity/src/main/java/it/unimol/Main.java
@@ -4,14 +4,18 @@ package it.unimol;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Formatter;
 import java.util.List;
 import java.util.Optional;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 public class Main {
 
+  private static final String TEMP_FILE_PATH = "temp";
   private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
   private static final MethodExtractor methodExtractor = new MethodExtractor();
 
@@ -20,47 +24,58 @@ public class Main {
       File file = new File(filePath);
       LOGGER.info("Processing file: " + file.getPath());
 
-      if (file.isDirectory()) {
-        List<File> subFiles = Arrays.asList(file.listFiles());
-        String[] subFilesPath = getSubFilesPaths(subFiles);
-        main(subFilesPath);
-      }
+      String projectName = file.getName(); // Ottieni solo il nome del progetto
+      getAllJavaFiles(file).forEach(f -> {
+        int projectIndex = f.getPath().indexOf(projectName);
+        System.out.println(f.getPath().substring(projectIndex));
+      });
 
-      Optional<String> extension = getFileExtension(file.getPath());
-      if (!extension.isPresent() || extension.isEmpty() ||
-          !extension.get().equals("java")) {
-        continue;
-      }
+      String projectTempPath = Paths.get(TEMP_FILE_PATH, projectName).toString();
+      // List<MethodInfo> methodsInfo = methodExtractor.extract(file);
 
-      List<MethodInfo> methodsInfo = methodExtractor.extract(file);
-
-      String os = System.getProperty("os.name");
-      String prefix = os.trim().contains("Windows") ? "temp\\" : "temp";
-      String tempFilePath = prefix + removeExtension(file.getPath());
-      System.out.println(tempFilePath);
-      String projectRootDir = System.getProperty("user.dir");
-      System.out.println(projectRootDir);
-
-      File tempDir = new File(tempFilePath);
-      if (!tempDir.exists()) {
-        tempDir.mkdirs();
-      }
-
-      System.setProperty("user.dir", tempFilePath);
-
-      for (MethodInfo methodInfo : methodsInfo) {
-        try (FileWriter writer = new FileWriter(methodInfo.getName() + ".java")) {
-          writer.write(
-              wrapAsSnippet(methodInfo.getBody(), methodInfo.getDeclaration()));
-        } catch (IOException e) {
-          e.printStackTrace();
-        }
-      }
-      System.setProperty("user.dir", projectRootDir);
+      // String projectRootDir = System.getProperty("user.dir");
+      // File tempDir = new File(TEMP_FILE_PATH);
+      // if (!tempDir.exists()) {
+      // tempDir.mkdirs();
+      // }
+      // System.setProperty("user.dir", tempDir.getAbsolutePath());
+      //
+      //
+      // for (MethodInfo methodInfo : methodsInfo) {
+      // try (FileWriter writer = new FileWriter(methodInfo.getName() +
+      // ".java")) {
+      // writer.write(
+      // wrapAsSnippet(methodInfo.getBody(),
+      // methodInfo.getDeclaration()));
+      // } catch (IOException e) {
+      // e.printStackTrace();
+      // }
+      // }
+      // System.setProperty("user.dir", projectRootDir);
     }
   }
 
-  private static void serialize(MethodInfo info) {
+  private static List<File> getAllJavaFiles(File rootDir) {
+    List<File> java_files = new ArrayList<>();
+    if (rootDir.isDirectory()) {
+      List<File> subFiles = Arrays.asList(rootDir.listFiles());
+      subFiles.forEach(file -> {
+        java_files.addAll(getAllJavaFiles(file));
+      });
+    }
+
+    Optional<String> extension = getFileExtension(rootDir.getPath());
+    if (!extension.isPresent() || extension.isEmpty() ||
+        !extension.get().equals("java")) {
+      return java_files;
+    }
+
+    java_files.add(rootDir);
+    return java_files;
+  }
+
+  private static String[] getSubFilesPaths(List<File> files) {
+    return files.stream().map(file -> file.getPath()).toArray(String[]::new);
   }
 
   private static Optional<String> getFileExtension(String filename) {
@@ -69,21 +84,27 @@ public class Main {
         .map(f -> f.substring(filename.lastIndexOf(".") + 1));
   }
 
-  private static String removeExtension(String filename) {
-    return filename.replaceFirst("[.][^.]+$", "");
-  }
-
-  private static String[] getSubFilesPaths(List<File> files) {
-    return files.stream().map(file -> file.getPath()).toArray(String[]::new);
-  }
-
   private static String wrapAsSnippet(String body, String declaration) {
     String nonFormattedMethod = declaration + body;
+    assert (!nonFormattedMethod.isEmpty() && nonFormattedMethod.contains("\n"));
+
     String spaces = nonFormattedMethod.split("\n")[0].replaceAll("[^ ]", "");
     StringBuilder method = new StringBuilder("");
     for (String str : nonFormattedMethod.split("\n")) {
       method.append(spaces + str + "\n");
     }
     return "class Snippet {\n" + method + "\n}";
+  }
+
+  private static String extractRelativePath(String filePath,
+      String projectName) {
+    Path fullPath = Paths.get(filePath);
+    Path projectPath = Paths.get(projectName);
+
+    // Calcola il percorso relativo
+    Path relativePath = projectPath.relativize(fullPath);
+
+    // Converte il percorso relativo in una stringa
+    return relativePath.toString();
   }
 }
