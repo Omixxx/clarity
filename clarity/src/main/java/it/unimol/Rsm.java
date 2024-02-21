0ad2abf434f11a7de3e15a34c807d9e661be7807
Omixxx <xxcortex99@gmail.com>
2024-02-21 14:54:44 +0100

    Minor improvements

--- clarity/src/main/java/it/unimol/Rsm.java
+++ clarity/src/main/java/it/unimol/Rsm.java
@@ -10,33 +10,28 @@ public class Rsm {
   private final String RSM_PATH = System.getProperty("user.dir") + "/lib/rsm.jar";
 
   public double analyze(Path path) {
+
+    String directoryPath = path.toAbsolutePath().toString();
+    ProcessBuilder processBuilder = new ProcessBuilder("java", "-jar", RSM_PATH, directoryPath);
+    processBuilder.directory(new File(System.getProperty("user.dir") + "/lib"));
+    StringBuilder output = new StringBuilder();
+
     try {
-      String directoryPath = path.toAbsolutePath().toString();
-
-      ProcessBuilder processBuilder = new ProcessBuilder("java", "-jar", RSM_PATH, directoryPath);
-      processBuilder.directory(
-          new File(System.getProperty("user.dir") + "/lib"));
-
       Process process = processBuilder.start();
       BufferedReader is = new BufferedReader(new InputStreamReader(process.getInputStream()));
-
-      StringBuilder output = new StringBuilder();
       String line;
       while ((line = is.readLine()) != null) {
         output.append(line);
       }
       process.waitFor();
-      return extractScore(output.toString());
     } catch (IOException | InterruptedException e) {
       e.printStackTrace();
     }
-    return 0;
+    return extractScore(output.toString());
   }
 
   private double extractScore(String output) {
-
     String[] parts = output.split("\\s+");
-    // Ottieni l'ultimo elemento dall'array
     String lastValue = parts[parts.length - 1];
     return Double.parseDouble(lastValue);
   }
